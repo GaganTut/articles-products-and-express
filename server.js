b96cee8b0084d62eeaf226863962c83441fb90e4
GaganTut <gstut@live.com>
2017-04-19 18:42:11 -1000

    Set up articles route

---
+++ server.js
@@ -0,0 +1,6 @@
+/*jshint esversion: 6*/
+const express = require('express');
+const app = express();
+const PORT = 8888;
+
+const server = app.listen(PORT);
---
+++ test/server.spec.js
@@ -0,0 +1,5 @@
+/*jshint esversion: 6*/
+const chai = require('chai');
+const request = require('supertest');
+
+const expect = chai.expect;
